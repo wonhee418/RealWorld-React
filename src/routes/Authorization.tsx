23a4c98511e1b28c7d3e0302f28832c3f78d05a2
WONHEE\dnjsg <cheeky4@naver.com>
2023-04-13 12:36:29 +0900

    feat : 유저 정보 확인하는 router 설정

--- src/routes/Authorization.tsx
+++ src/routes/Authorization.tsx
@@ -1,8 +1,6 @@
-import { useState } from "react";
 import { isLoggedInAtom } from "../atom/atom";
 import { useRecoilValue } from "recoil";
-import { useNavigate } from "react-router-dom";
-import Login from "../pages/Login";
+import { Navigate } from "react-router-dom";
 
 interface propsType {
   currentPath: string;
@@ -13,11 +11,10 @@ const Authorization = (props: propsType) => {
   const { currentPath, children } = props;
   // const [isLogged, routeToLoginPage] = useState();
   const isLogged = useRecoilValue(isLoggedInAtom);
-  const navigate = useNavigate();
 
   // 로그인되지 않은 경우에는 로그인 페이지로 리다이렉트
   if (!isLogged) {
-    return <Login></Login>;
+    return <Navigate to={"/login"} />;
   }
 
   return <>{children}</>;
